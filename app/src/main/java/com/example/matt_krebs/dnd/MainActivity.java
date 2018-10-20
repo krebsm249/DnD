7acfd92ae81a5ae5af34f0336eb1e83ecb343ff8
Krebsm249 <krebsm249@dupage.edu>
2018-10-20 18:02:37 -0500

    Testing Database with userID

--- app/src/main/java/com/example/matt_krebs/dnd/MainActivity.java
+++ app/src/main/java/com/example/matt_krebs/dnd/MainActivity.java
@@ -6,13 +6,19 @@ import android.view.*;
 import android.widget.*;
 import android.content.Intent;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.*;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
+
+    String val= "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +34,41 @@ public class MainActivity extends AppCompatActivity {
                 String newStr = "BALLS MATT KULLING";
 
                 final TextView helloTextView = (TextView) findViewById(R.id.textField);
-                helloTextView.setText(newStr);
+                //helloTextView.setText(newStr);
                 //databaseReference.setValue(newStr);
                 writeNewUser(databaseReference,"123","Matt","krebsmatt@ymail.com");
+
+               //helloTextView.setText(databaseReference.child("users").child("123").child("name").getKey());
+
+                List<String> arr = new ArrayList<String>();
+
+                arr.add("Test1");
+                arr.add("Test2");
+                arr.add("Test3");
+
+                databaseReference.child("test").setValue(arr);
+
+                DatabaseReference test = databaseReference;
+
+                for (int x = 0;x<3;x ++) {
+                    test = databaseReference.child("test").child(String.valueOf(x));
+                }
+
+                test.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        val += dataSnapshot.getValue(String.class);
+                        helloTextView.setText(val);
+                        val += ", ";
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                helloTextView.setText(val);
             }
         });
 
@@ -50,4 +88,7 @@ public class MainActivity extends AppCompatActivity {
 
         ref.child("users").child(userId).setValue(user);
     }
+
+
+
 }
