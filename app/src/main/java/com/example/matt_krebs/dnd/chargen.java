a1bde27c34166e3c4bdf5b54e0a74c0441b6a6d8
lubu42 <lubu42@wowway.com>
2018-10-20 15:28:22 -0500

    Test

---
+++ app/src/main/java/com/example/matt_krebs/dnd/chargen.java
@@ -0,0 +1,13 @@
+package com.example.matt_krebs.dnd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class chargen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chargen);
+    }
+}
