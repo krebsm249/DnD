e3ba34e2124099a67053c38197b728d093a7a4bf
Krebsm249 <krebsm249@dupage.edu>
2018-10-20 20:18:22 -0500

    Testing Database with userID

--- app/src/main/java/com/example/matt_krebs/dnd/chargen.java
+++ app/src/main/java/com/example/matt_krebs/dnd/chargen.java
@@ -1,11 +1,15 @@
 package com.example.matt_krebs.dnd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.*;
 import java.util.ArrayList;
 import java.util.List;
+import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,6 +24,9 @@ public class chargen extends AppCompatActivity {
     private List<String> raceArray = new ArrayList<String>();
 
     //private String[] raceArray = new String[3];
+
+    private String cardStatusString = "t";
+
 
 
 
@@ -57,5 +64,30 @@ public class chargen extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, raceArray);
         adp1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sp1.setAdapter(adp1);
+
+        sp1.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent,
+                                       View view, int pos, long id) {
+                cardStatusString = String.valueOf(pos);
+                Log.e("TEST1","testInLog");
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
+
+        final Button newCharBtn = findViewById(R.id.nextBtn);
+        newCharBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.e("Test", cardStatusString);
+            }
+        });
     }
 }
